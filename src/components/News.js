36da40b31d618fb42d37d1f73b11f4f94806aadf
Tayyab <40110511+tayyab-ash@users.noreply.github.com>
2023-09-01 12:51:02 +0500

    refoctorized the code - removed unnecessary code - fixed some major bugs i.e. page navigation was not working properly - all available news not loading etc and more

--- src/components/News.js
+++ src/components/News.js
@@ -4,69 +4,82 @@ import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 
 export class News extends Component {
-  static defaultProps ={
+  static defaultProps = {
     country: this.country,
-    pageSize: 20,
+    pageSize: this.pageSize,
     category: 'general'
 
   }
-  static propTypes ={
+  static propTypes = {
     country: PropTypes.string,
     pageSize: PropTypes.number,
     category: PropTypes.string
   }
 
 
-  constructor(){
+  constructor() {
     super();
     console.log("hello i am a Constructor.")
     this.state = {
-      articles : [],
+      articles: [],
       loading: false,
-      page:1
+      page: 1
     }
   }
 
-  componentDidUpdate(prevProps) {
+
+  async componentDidUpdate(prevProps) {
     if (prevProps.country !== this.props.country) {
+      this.setState({
+        page: 1
+      })
       this.componentDidMount();
     }
-  } 
-  async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=94c9334cf7384aa6a251f6dab17f9b39&page=1&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
+  }
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=94c9334cf7384aa6a251f6dab17f9b39&pageSize=${this.props.pageSize}&page=${this.state.page}`;
+    this.setState({ loading: true })
     let data = await fetch(url);
-    let parseData =await data.json();
+    let parseData = await data.json();
+    console.log(parseData);
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
       loading: false
     })
   }
-
-  handlePrevClick =async() =>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=94c9334cf7384aa6a251f6dab17f9b39&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parseData =await data.json();
-    this.setState({
-      articles: parseData.articles,
-      page: this.state.page-1,
-      loading: false
-    })
+  async componentDidMount() {
+    this.updateNews();
+  }
+  handlePrevClick = async () => {
+    if (this.state.page > 1) {
+      this.setState(
+        (prevState) => ({
+          page: prevState.page - 1,
+        }),
+        () => {
+          this.updateNews();
+        }
+      );
+    }
   }
 
-  handleNextClick =async() =>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&cetegory=${this.props.category}&apiKey=94c9334cf7384aa6a251f6dab17f9b39&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true})
-    let data = await fetch(url);
-    let parseData =await data.json();
-    this.setState({
-      articles: parseData.articles,
-      page: this.state.page+1,
-      loading: false
-    })
+  handleNextClick = async () => {
+    if (this.state.page < (Math.ceil(this.state.totalResults / this.props.pageSize))) {
+      this.setState(
+        (prevState) => ({
+          page: prevState.page + 1,
+        }),
+        () => {
+          this.updateNews();
+        }
+      );
+    }
   }
+
+
+
+
 
   render() {
     console.log("render")
@@ -74,19 +87,19 @@ export class News extends Component {
       <div className='container my-3'>
         <h1 className='text-center'>Top Headlines</h1>
         <hr />
-        {this.state.loading && <Spinner/>}
-       { !this.state.loading && <div className='d-flex justify-content-between'>
-            <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.handlePrevClick}>&#8592; Previous</button>
-            <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/20)} className='btn btn-dark' onClick={this.handleNextClick}>Next &#8594;</button>
+        {this.state.loading && <Spinner />}
+        {!this.state.loading && <div className='d-flex justify-content-between'>
+          <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.handlePrevClick}>&#8592; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className='btn btn-dark' onClick={this.handleNextClick}>Next &#8594;</button>
         </div>}
         <div className='d-flex justify-content-between flex-wrap'>
-          {!this.state.loading && this.state.articles.map((element)=>{
-             return <NewsItem key={element.url} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
-          })}  
+          {!this.state.loading && this.state.articles.map((element) => {
+            return <NewsItem key={!element.url ? Math.random() : element.url} title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} author={!element.author ? 'Unknown' : element.author} date={element.publishedAt} source={element.source.name} />
+          })}
         </div>
-        { !this.state.loading && <div className='d-flex justify-content-between'>
-            <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.handlePrevClick}>&#8592; Previous</button>
-            <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/20)} className='btn btn-dark' onClick={this.handleNextClick}>Next &#8594;</button>
+        {!this.state.loading && <div className='d-flex justify-content-between'>
+          <button disabled={this.state.page <= 1} className='btn btn-dark' onClick={this.handlePrevClick}>&#8592; Previous</button>
+          <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className='btn btn-dark' onClick={this.handleNextClick}>Next &#8594;</button>
         </div>}
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,16 +4,18 @@ import './NewsItem.css';
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props; 
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div>
         <div className="card my-3">
-          <img src={!imageUrl? 'https://i.ibb.co/JvHLYH0/News-APP-1.png':imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl ? 'https://i.ibb.co/JvHLYH0/News-APP-1.png' : imageUrl} className="card-img-top" alt="..." />
+          <p className="card-text mt-1 mb-0"><small className="mx-3">{source}</small></p>
           <div className="card-body">
-            <h5 className="card-title">{!title? 'Sorry! This news is not avaiable right now. Please refresh to update news catalog!' : title}</h5>
+            <h5 className="card-title">{!title ? 'Sorry! This news is not avaiable right now. Please refresh to update news catalog!' : title}</h5>
             <p className="card-text">
-              {!description? 'Description not available. Click read more for details.':description}
+              {!description ? 'Description not available. Click read more for details.' : description}
             </p>
+            <p className="card-text"><small className="text-body-secondary">{!(author && title) ? 'Not Available' : `By ${author} Updated on ${new Date(date).toGMTString()}`}</small></p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-dark">
               Read More
             </a>
